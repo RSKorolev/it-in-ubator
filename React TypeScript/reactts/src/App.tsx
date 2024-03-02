69e8e68d5b19854a3ae7e543ca302444c897e0e8
Ruslan Korolev <RSKorolev@bk.ru>
2024-03-03 00:08:09 +0700

    added type props for PageTitle

--- React TypeScript/reactts/src/App.tsx
+++ React TypeScript/reactts/src/App.tsx
@@ -20,7 +20,10 @@ function App() {
   );
 }
 
-export function PageTitle(props: any) {
+type PageTitlePropsType = {
+  title: string;
+};
+export function PageTitle(props: PageTitlePropsType) {
   return <h1>{props.title}</h1>;
 }
 export default App;
